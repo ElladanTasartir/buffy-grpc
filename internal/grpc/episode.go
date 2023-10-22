bbb36f7d59702f876db3aa73ce3e7e0cc250e2bc
Erick Malta <erickmalta100@gmail.com>
2023-10-22 14:43:48 -0300

    feat(internal): add season request endpoint

--- internal/grpc/episode.go
+++ internal/grpc/episode.go
@@ -8,24 +8,58 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-type EpisodesService struct{}
+type EpisodesService struct {
+	client *buffyClient.BuffyClient
+}
 
-func (es EpisodesService) GetEpisode(ctx context.Context, req *proto.EpisodeRequest) (*proto.EpisodeResponse, error) {
+func NewEpisodesService() (*EpisodesService, error) {
 	client, err := buffyClient.NewBuffyClient()
 	if err != nil {
-		err = status.New(codes.Unknown, err.Error()).Err()
 		return nil, err
 	}
 
-	episode, err := client.GetEpisode(req.Season, req.Episode)
+	return &EpisodesService{
+		client: client,
+	}, nil
+}
+
+func (es *EpisodesService) GetEpisode(ctx context.Context, req *proto.EpisodeRequest) (*proto.EpisodeResponse, error) {
+	episode, err := es.client.GetEpisode(req.Season, req.Episode)
 	if err != nil {
 		err = status.New(codes.NotFound, err.Error()).Err()
 		return nil, err
 	}
 
 	return &proto.EpisodeResponse{
-		Id:          episode.Id,
-		Name:        episode.Episode,
-		Description: episode.Description,
+		Episode: &proto.Episode{
+			Id:          episode.Id,
+			Name:        episode.Episode,
+			Description: episode.Description,
+			Screenshot:  episode.Screenshot,
+			Trivia:      episode.Trivia,
+		},
+	}, nil
+}
+
+func (es *EpisodesService) GetSeason(ctx context.Context, req *proto.SeasonRequest) (*proto.SeasonResponse, error) {
+	episodes, err := es.client.GetSeason(req.Season)
+	if err != nil {
+		err = status.New(codes.NotFound, err.Error()).Err()
+		return nil, err
+	}
+
+	var response []*proto.Episode
+	for _, episode := range episodes {
+		response = append(response, &proto.Episode{
+			Id:          episode.Id,
+			Name:        episode.Episode,
+			Description: episode.Description,
+			Screenshot:  episode.Screenshot,
+			Trivia:      episode.Trivia,
+		})
+	}
+
+	return &proto.SeasonResponse{
+		Episodes: response,
 	}, nil
 }
